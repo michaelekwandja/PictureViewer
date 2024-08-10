cdc220214d717ebbcb2051b0ea46848ea5da29d5
michaelekwandja <145231541+michaelekwandja@users.noreply.github.com>
2024-08-10 14:57:59 +0200

    Code to open a dialog box

--- PictureViewer1/Form1.Designer.cs
+++ PictureViewer1/Form1.Designer.cs
@@ -105,6 +105,7 @@
             this.showButton.TabIndex = 0;
             this.showButton.Text = "Show a picture";
             this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
             // 
             // clearButton
             // 
